1a964262ce760ea61221ae4f52f2fa3aab84bf7d
yusufyaman07 <yamanyusuf00000@gmail.com>
2024-01-29 16:02:05 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import Pokegame from './Components/Pokegame'
+import './Components/Pokecard.css'
+
+function App() {
+  return (
+    <div className='App'>
+      <h1>Pokemon Go Card Game</h1>
+      <Pokegame />
+    </div>
+  )
+}
+
+export default App
